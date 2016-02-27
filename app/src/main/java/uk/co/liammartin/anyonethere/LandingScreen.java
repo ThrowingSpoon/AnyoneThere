855bc2eccdd0351ad33abe3417b34aa3a103da28
Liam <liam@zoocha.com>
2016-02-27 18:58:41 +0000

    WORKING BASIC RECYCLERVIEW OF DATA FROM SERVER USING OKHTTP

--- app/src/main/java/uk/co/liammartin/anyonethere/LandingScreen.java
+++ app/src/main/java/uk/co/liammartin/anyonethere/LandingScreen.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -33,6 +34,7 @@ public class LandingScreen extends Activity {
     String data_url = "http://134.83.83.25:47309/Json";
     final String TAG = "LandingScreen.java";
     private List<user> users = new ArrayList<>();
+    String data;
 
     //OkHttpClient
     private final OkHttpClient client = new OkHttpClient();
@@ -41,10 +43,8 @@ public class LandingScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_screen);
-
         //Get the RecyclerView
         rv = (RecyclerView) findViewById(R.id.rv);
-
         //Getting the refresh button and attaching an onclick listener
         refresh_button = (Button) findViewById(R.id.refresh_button);
         refresh_button.setOnClickListener(new View.OnClickListener() {
@@ -53,17 +53,14 @@ public class LandingScreen extends Activity {
                 initializeData();
             }
         });
-
         //Create a LinearLayoutManager and set it to the RecyclerView
         //This will mean the RecyclerView will add items below eachother
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
         rv.setHasFixedSize(true);
-
         //Create an instance of RVAdapter so we can set up the RecyclerView using an adapter
         initializeAdapter();
-
-        //Create instances for data (just random test data at the moment)
+        //Pull the data down from the server and create data objects from them
         initializeData();
     }
 
@@ -72,13 +69,13 @@ public class LandingScreen extends Activity {
      */
     public void initializeData() {
         try {
-            run();
+            getUsernames();
+            //users.add(new user("TEST"));
         } catch (Exception e) {
             Log.d("123123", e.toString());
         }
-        rv.getAdapter().notifyItemRangeChanged(0, rv.getAdapter().getItemCount());
     }
-    
+
     /**
      * Create an instance of RVAdapter (Recycler View Adapter) using an ArrayList of data
      * objects, in this case we are using user objects (from user.java)
@@ -88,12 +85,15 @@ public class LandingScreen extends Activity {
         rv.setAdapter(adapter);
     }
 
-    public void run() throws Exception {
+    public void getUsernames() throws Exception {
+
         Request request = new Request.Builder()
                 .url("http://134.83.83.25:47309/Json")
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
+
+            String responseString;
 
             @Override
             public void onFailure(Call call, IOException e) {
@@ -103,35 +103,44 @@ public class LandingScreen extends Activity {
             //Getting our response
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                if (!response.isSuccessful())
+                    throw new IOException("Unexpected code " + response);
 
                 Headers responseHeaders = response.headers();
                 for (int i = 0; i < responseHeaders.size(); i++) {
                     System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
                 }
 
-                //parsing the json data
-                String jsonData = response.body().string();
-                try {
-                    usernames.clear();
-                    users.clear();
-                    JSONArray array = new JSONArray(jsonData);
-                    for (int i = 0; i < array.length(); i++) {
-                        JSONObject row = array.getJSONObject(i);
-                        Log.d(TAG, row.toString());
-                        Log.d(TAG, row.getString("USERNAME"));
-                        usernames.add(row.getString("USERNAME"));
+                responseString = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        displayUsernames(responseString);
+                        rv.getAdapter().notifyDataSetChanged();
                     }
-                    for (String username : usernames) {
-                        users.add(new user(username));
-                    }
-                    rv.getAdapter().notifyItemRangeChanged(0, rv.getAdapter().getItemCount());
-                } catch (Exception e) {
-                    Log.d("123123", e.toString());
-                }
-                
+                });
             }
-
         });
+
+    }
+
+    private void displayUsernames(String response) {
+        try {
+            usernames.clear();
+            users.clear();
+            JSONArray array = new JSONArray(response);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject row = array.getJSONObject(i);
+                Log.d(TAG, row.toString());
+                Log.d(TAG, row.getString("USERNAME"));
+                usernames.add(row.getString("USERNAME"));
+            }
+            for (String username : usernames) {
+                users.add(new user(username));
+            }
+            rv.getAdapter().notifyItemRangeChanged(0, rv.getAdapter().getItemCount());
+        } catch (Exception e) {
+            Log.d("123123", e.toString());
+        }
     }
 }
